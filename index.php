d88bf7d31f7282c3c7f21b474e7d22622c994686
dheerajkharwar102-ship-it <dheerajkharwar102@gmail.com>
2025-10-18 18:34:23 +0530

    Increase max-height of modal to 90vh

--- index.php
+++ index.php
@@ -1570,7 +1570,7 @@ $togle_temp_count = 0;
       box-shadow: 0 8px 24px rgba(0, 0, 0, 0.2);
       width: 90%;
       max-width: 700px;
-      max-height: 80vh;
+      max-height: 90vh;
       overflow: hidden;
    }
 
